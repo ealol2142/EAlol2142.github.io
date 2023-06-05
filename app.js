634bbfb308cd12493fa9252dd569ddc7b23ac350
rostislav <ealol2142@gmail.com>
2023-06-05 16:45:48 +0300

    s

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
 let item = "";
 let choice = "";
 
@@ -10,7 +13,7 @@ let boards = document.getElementById("Boards");
 let floor_til = document.getElementById("Floor_tiles");
 
 alum_pipes.addEventListener("click", function(){
-    item = "Al";
+    item = "Aluminum pipes";
 });
 
 boards.addEventListener("click", function(){
@@ -18,14 +21,16 @@ boards.addEventListener("click", function(){
 });
 
 floor_til.addEventListener("click", function(){
-    item = "Floortiles";
+    item = "Floor tiles";
 });
 
 btn1.addEventListener("click", () => {
-    data={
-        itemName: "Al",
+   
+    let data = {
+        name: item,
     }
-    tg.sendData(data);
+    tg.sendData(JSON.stringify(data));
+
     tg.close()
 });
 
