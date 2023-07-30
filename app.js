f2b5d526636308a1c3f94e60c9553475081807d6
ealol <komputer@MacBook-Air-komputer.local>
2023-07-30 17:57:13 +0300

    t

--- app.js
+++ app.js
@@ -2,36 +2,62 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
 
 let item = "";
-let choice = "";
 
-let alum_pipes = document.getElementById("Aluminum_pipes");
-let boards = document.getElementById("Boards");
-let floor_til = document.getElementById("Floor_tiles");
+let btn_pipe = document.getElementById("pipe");
+let btn_les = document.getElementById("les");
+let btn_plitka = document.getElementById("plitka");
+let btn_ugol = document.getElementById("ugol");
 
-alum_pipes.addEventListener("click", function(){
-    item = "Aluminum pipes";
-});
 
-boards.addEventListener("click", function(){
-    item = "Boards";
-});
-
-floor_til.addEventListener("click", function(){
-    item = "Floor tiles";
-});
-
-btn1.addEventListener("click", () => {
-   
+btn_pipe.addEventListener("click", () => {
     let data = {
-        name: item,
+        name: 'pipe',
     }
     tg.sendData(JSON.stringify(data));
-
     tg.close()
 });
+
+btn_les.addEventListener("click", () => {
+    let data = {
+        name: 'les',
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close()
+});
+
+btn_plitka.addEventListener("click", () => {
+    let data = {
+        name: 'plitka',
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close()
+});
+
+btn_ugol.addEventListener("click", () => {
+    let data = {
+        name: 'ugol',
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close()
+});
+
+// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+//     tg.sendData(item);
+// });
+
+// btn_pipe.addEventListener("click", function () {
+//     item = "pipe";
+// });
+// btn_les.addEventListener("click", function () {
+//     item = "les";
+// });
+// btn_plitka.addEventListener("click", function () {
+//     item = "plitka";
+// });
+// btn_ugol.addEventListener("click", function () {
+//     item = "ugol";
+// });
 
 
